74904a3ff7c03dc4d0f471c3144e5e8195f526c2
Aris <aris.chrysakis@gmail.com>
2019-03-25 10:13:48 +0100

    Completed the classify.py script.

--- classify.py
+++ classify.py
@@ -1,11 +1,12 @@
 import cv2
 import numpy as np
 from utils import image_to_vector, vector_to_image
+import pickle
 from sklearn.decomposition import PCA
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import LinearSVC
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import classification_report
+from sklearn.metrics import accuracy_score
 
 # Load training data
 directory = '../data/clean/'
@@ -15,8 +16,9 @@ input = data['inputs']
 labels = data['labels']
 
 # Principal components projection
-pca = PCA(n_components=15)
-projection = pca.fit_transform(input)
+pca = PCA(n_components=7)
+pca.fit(input)
+projection = pca.transform(input)
 
 # k-nearest neighbors training
 classifierKNN = KNeighborsClassifier(n_neighbors=2)
@@ -30,14 +32,23 @@ classifierSVM.fit(projection, labels)
 classifierLR = LogisticRegression()
 classifierLR.fit(projection, labels)
 
-# Load testing data
-pass
+# Load and project the testing data
+directory = '../data/clean/'
+with open(directory + 'test.pkl', 'rb') as input:
+    data = pickle.load(input)
+input = data['inputs']
+labels = data['labels']
+projection = pca.transform(input)
 
 # k-nearest neighbors testing
-pass
+predictions = classifierKNN.predict(projection)
+print(f"Accuracy: {accuracy_score(labels, predictions)}\n")
 
 # Support vector machine testing
-pass
+predictions = classifierSVM.predict(projection)
+print(f"Accuracy: {accuracy_score(labels, predictions)}\n")
 
 # Logistic regression testing
-pass
+predictions = classifierLR.predict(projection)
+print(f"Accuracy: {accuracy_score(labels, predictions)}\n")
+
--- explore.py
+++ explore.py
@@ -7,7 +7,6 @@ import pickle
 directory = '../data/clean/'
 with open(directory + 'train.pkl', 'rb') as input:
     data = pickle.load(input)
-
 input = data['inputs']
 labels = data['labels']
 
@@ -16,6 +15,19 @@ projection = pca.fit_transform(input)
 reconstruction = pca.inverse_transform(projection)
 print(pca.singular_values_ / np.sum(pca.singular_values_), '\n')
 print(1 - pca.explained_variance_ratio_)
+
+for i, image in enumerate(input):
+    combined = np.concatenate((vector_to_image(image),
+                               vector_to_image(reconstruction[i, :])), axis=1)
+    cv2.imshow('', combined)
+    cv2.waitKey(0)
+
+with open(directory + 'test.pkl', 'rb') as input:
+    data = pickle.load(input)
+input = data['inputs']
+labels = data['labels']
+projection = pca.transform(input)
+reconstruction = pca.inverse_transform(projection)
 
 for i, image in enumerate(input):
     combined = np.concatenate((vector_to_image(image),
